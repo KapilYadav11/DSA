2fe5cccfb8c835330f7c96d4f9671fc1485d774d
KapilYadav11 <kapilyadav951641@gmail.com>
2026-02-20 06:23:22 +0530

    inheritance

--- javax/oops/inheritance/Main.java
+++ javax/oops/inheritance/Main.java
@@ -3,8 +3,13 @@ package oops.inheritance;
 public class Main {
 
     public static void main(String [] args){
-        Box box1 = new Box(4.6,7.9,9.9);
-        Box box2 = new Box(box 1);
+        Box box1 = new Box(5, 8,6);
+        Box box2 = new Box(box1);
+       System.out.println(boxl.l + " " + box1.w + " " + box1.h);
+
+    BoxWeight box3 = new BoxWeight();
+    BoxWeight box4 = new BoxWeight(2, 8, 3, 8);
+    System.out.println(box3.h + " " + box3.weight);
     }
    
 }
