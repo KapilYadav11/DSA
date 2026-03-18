5d58fde32461f06fdc74d34c7ca769d2b5ffc7a9
KapilYadav11 <kapilyadav951641@gmail.com>
2026-03-18 23:51:00 +0530

    a2z strivers Question

---
+++ Strivers-sheet/string/commonPrefix.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class commonPrefix {
+    public String longestCommonPrefix(String[] v){
+        StringBuilder ans = new StringBuilder();
+
+        Arrays.sort(v);
+
+        String first  = v[0];
+        String last = v[v.length - 1];
+
+        for(int i = 0; i < Math.min(first.length(), last.length()); i++){
+            if(first.charAt(i) != last.charAt(i)){
+                return ans.toString();
+            }
+            ans.append(first.charAt(i));
+        }
+        return ans.toString();
+
+    }
+
+    public static void main(String [] args){
+        commonPrefix sol = new commonPrefix();
+        String[] input ={"interview", "internet", "internal", "interval"};
+
+        String result = sol.longestCommonPrefix(input);
+
+        
+        System.out.println("Longest Common Prefix: " + result); 
+    }
+}
