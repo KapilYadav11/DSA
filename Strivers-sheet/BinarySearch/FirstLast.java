f2cb6196ba6be1dd776c12979cd9bc5b1b6a36ff
KapilYadav11 <kapilyadav951641@gmail.com>
2026-04-11 20:58:47 +0530

    binary search

--- Strivers-sheet/BinarySearch/FirstLast.java
+++ Strivers-sheet/BinarySearch/FirstLast.java
@@ -1,38 +1,56 @@
 //Problem Statement: Given a sorted array of N integers, write a program to find the index of the last occurrence of the target key. If the target is not found then return -1. Note: Consider 0 based indexing
 
-import java.util.Arrays;
-import java.util.List;
-
 public class FirstLast {
-    
-    public int findFirstAndLast(int n, int key, List<Integer> v){
-        int start = 0;
-        int end = n - 1;
-        int res = -1;
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if(v.get(mid) == key){
+        return new int[]{first, last};
+        
+    }
+    private int findFirst(int[] nums, int target){
+        int low = 0, high = nums.length - 1;
+        int res = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
                 res = mid;
-                start = mid + 1;
+                high = mid - 1;
             }
-            else if (key < v.get(mid)) {
-                end = mid - 1;
+            else if(nums[mid] < target){
+                low = mid + 1;
             }
             else{
-                start = mid + 1;
+                high = mid - 1;
             }
-        }
-        return res;
-
+        }    
+            return res;
     }
-    
-    public static void main(String [] args){
-        int key = 13;
-        List<Integer> v = Arrays.asList(3, 4, 13, 13, 13, 20, 40);
-        int n = v.size();
+
+    private int findLast(int[] nums, int target){
+        int low = 0, high = nums.length - 1;
+        int res = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                res = mid;
+                low = mid + 1;
+            }
+            else if(nums[mid] < target){
+                    low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }        
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
         FirstLast sol = new FirstLast();
-        int ans = sol.findFirstAndLast(n, key, v);
-        System.out.println(ans);
+        int[] ans = sol.searchRange(nums, target);
+       System.out.println("The answer is: [" + ans[0] + ", " + ans[1] + "]");
     }
 }
