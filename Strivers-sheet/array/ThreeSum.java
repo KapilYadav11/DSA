ad54e1c3b4a5485ba89b36eb142ab8bb9117ceb3
KapilYadav11 <kapilyadav951641@gmail.com>
2026-06-24 22:09:19 +0530

    uyutrewdf

---
+++ Strivers-sheet/array/ThreeSum.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+  public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1, right = n - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right])); 
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return ans;
+    }
+    public static void main(String[] args) {
+        ThreeSum sol = new ThreeSum();
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        
+        List<List<Integer>> res = sol.threeSum(arr); 
+
+        for (List<Integer> triplet : res) {
+            for (int num : triplet) System.out.print(num + " ");
+            System.out.println();
+        }
+    }
+}
