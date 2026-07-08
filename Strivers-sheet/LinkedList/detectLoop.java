8085b52e2b8cc6749f1e6866c9094b1df5a54079
KapilYadav11 <kapilyadav951641@gmail.com>
2026-07-08 19:15:56 +0530

    detect loop brute force

--- Strivers-sheet/LinkedList/detectLoop.java
+++ Strivers-sheet/LinkedList/detectLoop.java
@@ -1,17 +1,60 @@
-class ListNode {
-    int val;
-    ListNode next;
+import java.util.*;
 
-    ListNode(int val){
-        this.val = val; 
+class Node {
+    int data;
+    Node next;
+
+    Node(int data1, Node next1) {
+        data = data1;
+        next = next1;
     }
 
-    ListNode(int val, ListNode next){
-        this.val = val;
-        this.next = next;
-    }    
+    Node(int data1) {
+        data = data1;
+        next = null;
+    }
 }
-public class detectLoop {
-    public static ListNode findDetectLoop(ListNode head){}
-    
+
+public class DetectLoop {
+   
+    public boolean detectLoop(Node head) {
+        Node temp = head;
+        HashMap<Node, Integer> nodeMap = new HashMap<>();
+
+        while (temp != null) {
+           
+            if (nodeMap.containsKey(temp)) {
+                return true;
+            }
+           
+            nodeMap.put(temp, 1);
+
+           
+            temp = temp.next;
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+      
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        Node fourth = new Node(4);
+        Node fifth = new Node(5);
+
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        
+        fifth.next = third;
+
+        DetectLoop sol = new DetectLoop();
+        if (sol.detectLoop(head)) {
+            System.out.println("Loop detected in the linked list.");
+        } else {
+            System.out.println("No loop detected in the linked list.");
+        }
+    }
 }
